dcc6a44a0abbb2d35839b6fdc809f872b17be41d
Jannes <jurie.germishuys@combine.se>
2021-02-15 12:12:48 +0100

    Fix vid/img confusion

--- app_frontend.py
+++ app_frontend.py
@@ -158,18 +158,19 @@ def run_the_app():
             "**YOLO v3 Model** (overlap `%3.1f`) (confidence `%3.1f`)"
             % (overlap_threshold, confidence_threshold)
         )
-        st.video(processed_image)
+        with open(
+                    f"temp_{selected_frame}", "wb"
+                ) as out_file:  # open for [w]riting as [b]inary
+                    out_file.write(processed_image.tobytes())
+        st.video(f"temp_{selected_frame}")
         #os.remove(selected_frame)
     else:
         # Draw the header and image.
         st.subheader("Model Output")
         st.markdown("**YOLO v3 Model** (overlap `%3.1f`) (confidence `%3.1f`)"
             % (overlap_threshold, confidence_threshold))
-        with open(
-                    f"temp_{selected_frame}", "wb"
-                ) as out_file:  # open for [w]riting as [b]inary
-                    out_file.write(processed_image.tobytes())
-        st.image(f"temp_{selected_frame}", use_column_width=True)
+        
+        st.image(processed_image, use_column_width=True)
         #os.remove(selected_frame)
 
 
