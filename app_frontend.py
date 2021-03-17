bc97f88a1633b6846caed75571ddcc2b2a1704c7
Jannes Germishuys <jurie.germishuys@combine.se>
2021-03-17 14:16:12 +0100

    Change title size

--- app_frontend.py
+++ app_frontend.py
@@ -17,16 +17,16 @@ backend = "http://fdc2b62d293d.ngrok.io"
 
 def main():
     # Set up appearance of sidebar
-    st.sidebar.title("Koster Lab - Deep Sea Coral Detection")
+    st.sidebar.subtitle("Koster Lab - Deep Sea Coral Detection")
     st.sidebar.image(
         "https://panoptes-uploads.zooniverse.org/production/project_avatar/86c23ca7-bbaa-4e84-8d8a-876819551431.png",
-        use_column_width=False,
+        use_column_width=True,
     )
     # Run main app
     run_the_app()
     st.sidebar.image(
         "https://panoptes-uploads.zooniverse.org/production/project_attached_image/99429003-51ae-4667-b9b0-7ec2ff518723.png",
-        use_column_width=False,
+        use_column_width=True,
     )
 
 
