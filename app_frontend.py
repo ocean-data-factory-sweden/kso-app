88a141151804fe5ed8cf03019a63a8dd2b49b0fb
Jannes Germishuys <jurie.germishuys@combine.se>
2021-03-19 15:04:22 +0100

    Swap green and blue

--- app_frontend.py
+++ app_frontend.py
@@ -255,7 +255,8 @@ def run_the_app():
         selected_frame_number = movie_frames.iloc[selected_frame_index]
         selected_frame = get_movie_frame(selected_movie_path, selected_frame_number)
 
-        #selected_frame = np.float32(selected_frame)
+        selected_frame = green_blue_swap(selected_frame)
+        #selected_frame = cv2.cvtColor(selected_frame, cv2.COLOR_BGR2RGB)
         # Save in a temp file as YOLO expects filepath
         mbase = os.path.basename(selected_movie_path).split(".")[0]
         cv2.imwrite(f"{mbase}_{selected_frame_number}.jpeg", selected_frame)
